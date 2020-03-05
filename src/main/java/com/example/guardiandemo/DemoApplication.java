17a7a900212cfad1b6164c16166e925a2da38d34
raviassis <ravi.g.assis@gmail.com>
2020-03-04 21:50:21 -0300

    first commit

---
+++ src/main/java/com/example/guardiandemo/DemoApplication.java
@@ -0,0 +1,19 @@
+package com.example.guardiandemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	@RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Hello World!";
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
